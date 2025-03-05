13499f23cec6bf971d5cc42bf0bd94d2352c0587
Vidhi Goyal <109031897+vidhi1812@users.noreply.github.com>
2025-03-05 12:47:04 +0530

    backend

--- attendance/backend/index.js
+++ attendance/backend/index.js
@@ -1,21 +1,17 @@
 import express from 'express'
-import mongoose from 'mongoose';
 import dotenv from "dotenv"
-const app = express()
 dotenv.config();
+import ConnectMongo from "./Db.js"
+const app = express()
+
 const PORT=process.env.PORT || 4000;
-const URI=process.env.MongoDBURI
 
 
+ConnectMongo();
 
 //   connection of MongoDB 
 
-try {
-    mongoose.connect(URI);
-    console.log("connect to the mongodb");
-} catch (error) {
-    
-}
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
